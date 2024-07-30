1bd556e199625360a45d9bc293cbe7f843bc040a
Martin Knoetze <mknoetze@carbonlink.com.au>
2024-07-30 12:10:43 +1000

    Adds keybind to disable inline diagnostics and diagnostics

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,3 +37,37 @@ end
 
 set_filetype({ "docker-compose.yaml" }, "yaml.docker-compose")
 set_filetype({ "*.make" }, "makefile")
+
+
+-- Command to toggle inline diagnostics
+vim.api.nvim_create_user_command(
+  'DiagnosticsToggleVirtualText',
+  function()
+    local current_value = vim.diagnostic.config().virtual_text
+    if current_value then
+      vim.diagnostic.config({virtual_text = false})
+    else
+      vim.diagnostic.config({virtual_text = true})
+    end
+  end,
+  {}
+)
+
+-- Command to toggle diagnostics
+vim.api.nvim_create_user_command(
+  'DiagnosticsToggle',
+  function()
+    local current_value = vim.diagnostic.is_disabled()
+    if current_value then
+      vim.diagnostic.enable()
+    else
+      vim.diagnostic.disable()
+    end
+  end,
+  {}
+)
+
+vim.api.nvim_set_keymap('n', '<Leader>li', ':lua vim.cmd("DiagnosticsToggleVirtualText")<CR>', { noremap = true, silent = true, desc="Toggle Inline Comments" })
+
+vim.api.nvim_set_keymap('n', '<Leader>lI', ':lua vim.cmd("DiagnosticsToggle")<CR>', { noremap = true, silent = true, desc="Toggle Diagnostics" })
+
